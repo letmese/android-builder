eef23a7ec1965e2c81464304863538510a51ca8e
letmese <letmese@users.noreply.github.com>
2026-08-24 21:16:55 +0500

    THE FIX: add missing super.onCreate() - found via emulator testing

--- app/src/main/java/com/letmese/aikeyboard/MainActivity.java
+++ app/src/main/java/com/letmese/aikeyboard/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
             @Override
             public void uncaughtException(Thread t, Throwable e) {
